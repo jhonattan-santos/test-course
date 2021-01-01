ee511052f2d08fd74e52ba120d454f44037e0688
Jhonattan Santos <jhonattanwesleysantos@gmail.com>
2021-01-01 18:43:17 -0300

    (feat) adding query string to object test and method

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -7,3 +7,16 @@ const keyValueToString = ([key, value]) => {
 
 module.exports.queryString = obj =>
   Object.entries(obj).map(keyValueToString).join('&');
+
+module.exports.parse = string =>
+  Object.fromEntries(
+    string.split('&').map(item => {
+      let [key, value] = item.split('=');
+
+      if (value.indexOf(',') > -1) {
+        value = value.split(',');
+      }
+      console.log([key, value]);
+      return [key, value];
+    }),
+  );
--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,4 +1,4 @@
-const { queryString } = require('./queryString');
+const { queryString, parse } = require('./queryString');
 
 describe('Object to query string', () => {
   it('should create a valid query string when an object is provided', () => {
@@ -15,6 +15,7 @@ describe('Object to query string', () => {
       name: 'Jhonattan',
       abilities: ['JS', 'Angular'],
     };
+
     expect(queryString(obj)).toBe('name=Jhonattan&abilities=JS,Angular');
   });
 
@@ -26,8 +27,37 @@ describe('Object to query string', () => {
         second: 'Angular',
       },
     };
+
     expect(() => {
       queryString(obj);
     }).toThrowError();
+  });
+});
+
+describe('Query string to object', () => {
+  it('should convert a query string to object', () => {
+    const qs = 'name=Jhonattan&profession=developer';
+
+    expect(parse(qs)).toEqual({
+      name: 'Jhonattan',
+      profession: 'developer',
+    });
+  });
+
+  it('should convert a query string  of a single ky-value to object', () => {
+    const qs = 'name=Jhonattan';
+
+    expect(parse(qs)).toEqual({
+      name: 'Jhonattan',
+    });
+  });
+
+  it('should convert a query string to an object taking care of comma separated values', () => {
+    const qs = 'name=Jhonattan&abilities=JS,Angular';
+
+    expect(parse(qs)).toEqual({
+      name: 'Jhonattan',
+      abilities: ['JS', 'Angular'],
+    });
   });
 });
